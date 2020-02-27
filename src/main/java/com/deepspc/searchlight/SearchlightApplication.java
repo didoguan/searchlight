a1b453186f80b9c852fb126c52e40de2d6e90bdb
didoguan@163.com <didoguan@163.com>
2020-02-27 18:15:32 +0800

    新项目

---
+++ src/main/java/com/deepspc/searchlight/SearchlightApplication.java
@@ -0,0 +1,13 @@
+package com.deepspc.searchlight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchlightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchlightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deepspc/searchlight/SearchlightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepspc.searchlight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchlightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
